6e3eb7bb5be74a2a64da9901d79f0bfef44823ae
Egor_Trubilo <trubilooficial@gmail.com>
2021-12-17 20:47:17 +0300

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, {useEffect, useState} from 'react'
 import PostList from "./Components/PostList";
-
 import PostForm from "./Components/PostForm";
 import styled from "styled-components";
 import PostFilter from "./Components/PostFilter";
@@ -11,7 +10,8 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./Components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
-
+import {getPagesArray, getPagesCount} from "./utils/pages";
+import Paginator from "./Components/UI/pagination/Pagination";
 
 
 const StyledApp = styled.div`
@@ -24,17 +24,23 @@ function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: '',})
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const[fetchPosts, isPostLoading, postError] = useFetching(async ()=> {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+
+
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPagesCount(totalCount, limit))
     })
 
 
-
-    useEffect(()=> {
+    useEffect(() => {
         fetchPosts()
-    }, [])
+    }, [page])
 
 
     const createPost = (newPost) => {
@@ -46,6 +52,10 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
 
+    }
+
+    const changePage = (page) => {
+      setPage(page)
     }
 
     return (
@@ -66,12 +76,12 @@ function App() {
             {postError && <h1> Ошибка! ${postError}</h1>
             }
             {isPostLoading
-            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: "50px"}}>
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: "50px"}}>
                     <Loader/>
-            </div>
-            : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Мои посты'}/>
+                </div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Мои посты'}/>
             }
-
+<Paginator page={page} changePage={changePage} totalPages={totalPages}/>
 
         </StyledApp>
 
